3f23bb93e45ea1be3396c269235e10e75ff981b7
Matthew Thompson <mat@matthewjthompson.co.uk>
2020-11-15 18:47:55 +0000

    refactor: move timer functionality into a custom hook

--- src/timer/Timer.tsx
+++ src/timer/Timer.tsx
@@ -1,44 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import { Time } from './Time';
+import { useTimer } from './useTimer';
 
 export const Timer = () => {
     const duration = 5 * 60 * 1000;
 
-    const [remaining, setRemaining] = useState(duration);
-    const [endTime, setEndTime] = useState(0);
-    const [active, setActive] = useState(false);
-
-    const requestRef = React.useRef(0);
-
-    const animateTime = useCallback(
-        (time: number) => {
-            requestRef.current = requestAnimationFrame(animateTime);
-            setRemaining(endTime - time);
-        },
-        [endTime],
-    );
-
-    useEffect(() => {
-        if (active) {
-            requestRef.current = requestAnimationFrame(animateTime);
-        }
-
-        return () => cancelAnimationFrame(requestRef.current);
-    }, [active, animateTime]);
-
-    const start = () => {
-        setActive(true);
-        setEndTime(Date.now() + duration);
-    };
-
-    const stop = () => {
-        setActive(false);
-        cancelAnimationFrame(requestRef.current);
-        setRemaining(duration);
-    };
+    const { remaining, start, stop } = useTimer(duration);
 
     return (
         <View style={styles.container}>
---
+++ src/timer/useTimer.ts
@@ -0,0 +1,39 @@
+import React, { useState, useCallback, useEffect } from "react";
+
+export function useTimer(duration: number) {
+
+    const [remaining, setRemaining] = useState(duration);
+    const [endTime, setEndTime] = useState(0);
+    const [active, setActive] = useState(false);
+
+    const requestRef = React.useRef(0);
+
+    const animateTime = useCallback(
+        (time: number) => {
+            requestRef.current = requestAnimationFrame(animateTime);
+            setRemaining(endTime - time);
+        },
+        [endTime],
+    );
+
+    useEffect(() => {
+        if (active) {
+            requestRef.current = requestAnimationFrame(animateTime);
+        }
+
+        return () => cancelAnimationFrame(requestRef.current);
+    }, [active, animateTime]);
+
+    const start = () => {
+        setActive(true);
+        setEndTime(Date.now() + duration);
+    };
+
+    const stop = () => {
+        setActive(false);
+        cancelAnimationFrame(requestRef.current);
+        setRemaining(duration);
+    };
+
+    return { remaining, start, stop }
+}
